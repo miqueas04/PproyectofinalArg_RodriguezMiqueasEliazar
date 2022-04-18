eeac0a0ab096dc23500fcdbef7412191b7c854b7
miqueas04 <80845205+miqueas04@users.noreply.github.com>
2022-04-18 20:00:27 -0300

    bootstrap agreado mas header

--- proyectoF/src/app/app.module.ts
+++ proyectoF/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
