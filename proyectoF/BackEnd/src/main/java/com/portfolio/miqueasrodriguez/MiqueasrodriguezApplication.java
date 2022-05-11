89f3040346ff976d2a27b213f52ba1502edba9d3
miqueas04 <80845205+miqueas04@users.noreply.github.com>
2022-05-11 20:35:33 -0300

    creacion carpeta backend

---
+++ proyectoF/BackEnd/src/main/java/com/portfolio/miqueasrodriguez/MiqueasrodriguezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.miqueasrodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiqueasrodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiqueasrodriguezApplication.class, args);
+	}
+
+}
---
+++ proyectoF/BackEnd/src/test/java/com/portfolio/miqueasrodriguez/MiqueasrodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.miqueasrodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiqueasrodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
